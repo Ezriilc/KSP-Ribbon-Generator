2f0a2b99e00cf098a64f56df49b171be3c4d0694
Ezriilc <github@swiftek.net>
2014-11-12 18:26:27 -0500

    Added images, fleshing out .php.

--- index.php
+++ index.php
@@ -2,106 +2,12 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>KSP Ribbons SANDBOX</title>
-    <link rel="stylesheet" type="text/css" href="ribbons_SANDBOX.css"/>
+    <title>KSP Ribbons</title>
+    <link rel="stylesheet" type="text/css" href="ribbons.css"/>
     <script type="text/javascript" src="jquery-2.1.1.js"></script>
-    <script type="text/javascript" src="ribbons_SANDBOX.js"></script>
+    <script type="text/javascript" src="ribbons.js"></script>
 </head>
 <body>
-    <div class="ribbons">
-        <span class="ribbon-Grand_Tour ribbon" style="display:inline-block; border:1px solid black;">
-            ribbon-Grand_Tour
-            <br/>
-            <br/>
-            <span class="effect-Ribbon effect">Ribbon</span>
-            <span class="effect-HC effect">HC</span>
-            <span class="effect-Lighten_Bevel effect">Bevel</span>
-            <br/>
-            <br/>
-            <span class="device-Orbit_1 device">Orbit_1</span>
-            <br/>
-            <br/>5x:
-            <?php
-                for( $i=2; $i <= 8; $i++ ){
-                    echo '<span class="device-Orbit_'.$i.'_Silver device">'.$i.'</span>';
-                }
-            ?>
-            <br/>
-            <br/>1x:
-            <?php
-                for( $i=2; $i <= 8; $i++ ){
-                    echo '<span class="device-Orbit_'.$i.' device">'.$i.'</span>';
-                }
-            ?>
-            <br/>
-            <br/>
-            <span class="device-Aircraft device">Aircraft</span>
-            <span class="device-Multi-Part_Ship device">Multi-Part Ship</span>
-        </span>
-    </div>
-    <form class="ribbons" method="post"><fieldset>
-        <div>
-            Effects:
-            <div>
-                <label for="id-5">None</label>
-                <input id="id-5" type="radio" name="effects/type" value="None" checked="checked"/>
-            </div>
-            <div>
-                <label for="id-6">Ribbon</label>
-                <input id="id-6" type="radio" name="effects/type" value="Ribbon"/>
-            </div>
-            <div>
-                <label for="id-8">HC</label>
-                <input id="id-8" type="radio" name="effects/type" value="HC"/>
-            </div>
-            <div>
-                <label for="id-7">Lighten Bevel</label>
-                <input id="id-7" type="checkbox" name="effects/Lighten_Bevel"/>
-            </div>
-        </div>
-        <div class="planet-Grand_Tour planet">
-            <hr/>
-            planet-Grand_Tour planet
-            <div>
-                <label for="id-0">Achieved</label>
-                <input id="id-0" type="checkbox" name="Grand_Tour/Achieved"/>
-            </div>
-            <div>
-                <label for="id-1">Orbits</label>
-                <select id="id-1" name="Grand_Tour/Orbits">
-                    <option selected="selected">0</option>
-                    <?php
-                        for( $i=1; $i <= 16; $i++ ){
-                            echo '<option>'.$i.'</option>\r\n';
-                        }
-                    ?>
-                </select>
-            </div>
-            <div>
-                Craft:
-                <div>
-                    <label for="id-2">None</label>
-                    <input id="id-2" type="radio" name="Grand_Tour/craft" value="None" checked="checked"/>
-                </div>
-                <div>
-                    <label for="id-3">Aircraft</label>
-                    <input id="id-3" type="radio" name="Grand_Tour/craft" value="Aircraft"/>
-                </div>
-                <div>
-                    <label for="id-4">Multi-Part Ship</label>
-                    <input id="id-4" type="radio" name="Grand_Tour/craft" value="Multi-Part Ship"/>
-                </div>
-            </div>
-            <hr/>
-        </div>
-        <input type="submit" style=""/>
-    </fieldset></form>
-
-    <pre class="debug_display" style="
-        position:fixed;
-        top:0;right:0;
-        background-color:white;
-    ">debug_display</pre>
-    <?php var_dump(@$_POST); ?>
+<?php include('ribbons.php'); ?>
 </body>
 </html>
---
+++ ribbons.php
@@ -0,0 +1,273 @@
+<?php
+
+new RIBBONS;
+//var_dump(@$_SESSION['ribbons']['post']);
+echo RIBBONS::$output;
+//return RIBBONS::$output;
+
+class RIBBONS{
+    static
+    $db_file = './_gitignore/kerbaltek.sqlite3'
+    ,$images_root = './KSP_images'
+    ,$ribbons_table = 'ribbons'
+    ,$bad_db = '<p class="error message">Database failure.  Please try again.</p>'
+    ,$output = null
+    ,$dbcnnx = null
+    ,$planets = null
+    ,$layout = null
+    ,$asteroids = null
+    ,$devices = null
+    ,$devices_ordered = null
+    ,$effects = null
+    ;
+    // ALL SOI bodies are called "planets" here.
+    
+    function __construct(){
+        static::$planets = array( // In order of display, by column > row.
+            'Kerbol'   =>'0010001'
+            ,'Moho'	    =>'1000001'
+            ,'Asteroid' =>'1010010'
+            ,'Eve'      =>'1110101'
+            ,'Gilly'    =>'1010010'
+            ,'Eeloo'    =>'1010000'
+            ,'Kerbin'   =>'1111101'
+            ,'Mun'      =>'1001000'
+            ,'Minmus'   =>'1011010'
+            ,'Duna'     =>'1111001'
+            ,'Ike'      =>'1011010'
+            ,'Dres'     =>'1010000'
+            ,'Jool'     =>'0110001'
+            ,'Laythe'   =>'1100000'
+            ,'Vall'     =>'1000000'
+            ,'Tylo'     =>'1001100'
+            ,'Bop'      =>'1001010'
+            ,'Pol'      =>'1000010'
+            ,'Grand Tour' =>''
+        );
+        $planet_attributes = array('surface', 'atmosphere', 'geosynchronous', 'anomaly', 'challenge', 'eeva', 'asteroid'); // Number strings above.
+        foreach( static::$planets as $planet => $attribs ){
+            static::$planets[$planet] = array();
+            foreach( $planet_attributes as $key => $val ){
+                static::$planets[$planet][$val] = !!@$attribs[$key]; // Strings are ~like arrays.
+            }
+        }
+        
+        static::$effects = array( // In order of display.
+            'types' => array(
+                'Ribbon'
+                ,'High Contrast Ribbons'
+                ,'Lightened HC'
+                ,'Dense HC'
+                ,'Lightened Dense HC'
+            )
+            ,'singles' => array(
+                'Darken Bevel'
+                ,'Lighten Bevel'
+            )
+        );
+        
+        // Devices in order of inputs display: Category => Type => Device => Priority, Description.
+        static::$devices = array(
+            'mans' => array( // Category
+                'common' => array( // Type
+                    'Orbit'             => array(8, 'Periapsis above the atmosphere and apoapsis within the sphere of influence.')
+                    ,'Equatorial'       => array(5, 'Inclination less than 5 degrees and, apoapsis and periapsis within 5% of each other.')
+                    ,'Polar'            => array(4, 'Polar orbit capable of surveying the entire surface.')
+                    ,'Rendezvous'       => array(6, 'Docked two craft in orbit, or maneuvered two orbiting craft within 100m for a sustained time.')
+                )
+                ,'surface' => array(
+                    'Land Nav'          => array(9, 'Ground travel at least 30km or 1/5ths a world\'s circumference (whichever is shorter).')
+                )
+                ,'atmosphere' => array(
+                    'Atmosphere'        => array(3, 'Controlled maneuvers using wings or similar. Granted only if craft can land and then take off, or perform maneuvers and then attain orbit.')
+                )
+                ,'special' => array(
+                    'Geosynchronous'    => array(7, 'Achieve geosynchronous orbit around the world; or drag the body into geosynchronous orbit around another; or construct a structured, line-of-sight satellite network covering a specific location.')
+                    ,'Kerbol Escape'    => array(10, 'Achieved solar escape velocity - for Kerbol only.')
+                )
+            )
+            ,'crafts' => array(
+                'common' => array(
+                    'Probe'             => array(0, 'Autonomous craft which does not land.')
+                    ,'Capsule'          => array(0, 'Manned craft which does not land, or only performs a single, uncontrolled landing.')
+                    ,'Resource'         => array(0, 'Installation on the surface or in orbit, capable of mining and/or processing resources.')
+                    ,'Aircraft'         => array(0, 'Winged craft capable of atmospheric flight, with or without any atmosphere - does not grant Flight Wings device.')
+                    ,'Multi-Part Ship'  => array(0, 'Orbital vessel capable of docking and long-term habitation by multiple Kerbals.')
+                    ,'Station'          => array(0, 'A craft constructed from multiple parts in orbit.')
+                    ,'Armada'           => array(0, 'Three or more vessels, staged in orbit for a trip to another world, and launched within one week during one encounter window.')
+                    ,'Armada 2'         => array(0, 'Three or more vessels, staged in orbit for a trip to another world, and launched within one week during one encounter window.')
+                )
+                ,'surface' => array(
+                    'Impactor'          => array(0, 'Craft was destroyed by atmospheric or surface friction.')
+                    ,'Probe Lander'     => array(0, 'Autonomous craft which landed on a world\'s surface.')
+                    ,'Probe Rover'      => array(0, 'Autonomous craft which landed and performed controlled surface travel.')
+                    ,'Flag or Monument' => array(0, 'A marker left on the world.')
+                    ,'Lander'           => array(0, 'A craft carrying one or more Kerbals which landed without damage.')
+                    ,'Rover'            => array(0, 'A vehicle which landed and then carried one or more Kerbals across the surface of the world.')
+                    ,'Base'             => array(0, 'A permanent ground construction capable of long-term habitation by multiple Kerbals')
+                    ,'Base 2'           => array(0, 'A permanent ground construction capable of long-term habitation by multiple Kerbals')
+                )
+                ,'atmosphere' => array(
+                    'Meteor'            => array(0, 'Craft was destroyed due to atmospheric entry.')
+                )
+                ,'special' => array(
+                    'Extreme EVA'       => array(0, 'Landed and returned to orbit without the aid of a spacecraft.')
+                )
+            )
+            ,'misc' => array(
+                'common' => array(
+                    'Kerbal Lost'       => array(0, 'A Kerbal was killed or lost beyond the possibility of rescue.')
+                    ,'Kerbal Saved'     => array(1, 'Returned a previously stranded Kerbal safely to Kerbin.')
+                    ,'Return Chevron'   => array(12, 'Returned any craft safely to Kerbin from the world.')
+                )
+                ,'special' => array(
+                    'Anomaly'           => array(2, 'Discovered and closely inspected a genuine Anomaly.')
+                    ,'Challenge Wreath' => array(11, 'A special challenge for each world.')
+                )
+            )
+        );
+        static::$devices_ordered = array();
+        foreach( static::$devices as $cat => $types ){
+            if( $cat === 'crafts' ){ continue; }
+            foreach( $types as $type => $devices ){
+                foreach( $devices as $device => $details ){
+                    static::$devices_ordered[$details[0]] = array($device, $type, $cat, $details[1]);
+                }
+            }
+        }
+        ksort(static::$devices_ordered);
+        foreach( static::$devices['crafts'] as $type => $crafts ){
+            foreach( $crafts as $craft => $details ){
+                static::$devices_ordered[] = array($craft, $type, $cat, $details[1]);
+            }
+        }
+        
+        static::$output .= $this->get_form();
+        
+    }
+    
+    private function get_form(){
+        $form = '';
+        $form .= '
+<div class="ribbons">';
+        $i=0;
+        foreach( static::$planets as $planet => $attribs ){
+            $i++;
+            if( ($i-1) % 3 === 0 ){
+                if( $i > 1 ){
+                    $form .= '
+    </div>';
+                }
+                $form .= '
+    <div style="float:left; text-align:center; width:120px;">';
+            }
+            $image = static::$images_root.'/ribbons/'.$planet.'.png';
+            $height = '32px';
+            if( $planet === 'Grand Tour' ){
+                $image = static::$images_root.'/ribbons/shield/Base Colours.png';
+                $height = '96px';
+            }
+            
+            if( is_readable($image) && ! is_dir($image) ){
+                $image = ' background-image:url(\''.$image.'\');';
+            }else{ $image = ''; }
+            $form .= '
+        <div class="ribbon" style="position:relative; width:100%;height:'.$height.';line-height:'.$height.';'.$image.'">';
+            // Ribbon guts.
+            foreach( static::$effects as $val ){
+                foreach( $val as $effect ){
+                    $image = static::$images_root.'/ribbons';
+                    if( $planet === 'Grand Tour' ){ $image .= '/shield'; }
+                    $image .= '/'.$effect.'.png';
+                    if( is_readable($image) && ! is_dir($image) ){
+                        $display = ' display:none;';
+//                        $display = '';
+                        if(
+                            $effect === 'Ribbon'
+                            || @$selected // Check for default or posted value.
+                        ){ $display = ''; }
+                        $image = '
+            <img alt="'.$image.'" src="'.$image.'" style="position:absolute;top:0;left:0;'.$display.'"/>';
+                    }else{ $image = ''; }
+                    $form .= $image;
+                }
+            }
+            
+            // Devices in order of display: Category => Type => Device => Priority, Description.
+            foreach( static::$devices_ordered as $device ){
+                $name = $device[0];
+                $type = $device[1];
+                $cat = $device[2];
+                $desc = $device[3];
+                if(
+                    $type !== 'common'
+                    && empty( static::$planets[$planet][$type] )
+                    && $planet !== 'Grand Tour'
+                ){ continue; }
+                $image = static::$images_root.'/ribbons';
+                if( $planet === 'Grand Tour' ){ $image .= '/shield'; }
+                $image .= '/'.$name.'.png';
+                if( is_readable($image) && ! is_dir($image) ){
+                    $display = ' display:none;';
+//                    $display = '';
+                    if(
+                        @$selected // Check for default or posted value.
+                    ){ $display = ''; }
+                    $image = '
+            <img alt="'.$image.'" src="'.$image.'" style="position:absolute;top:0;left:0;'.$display.'"/>';
+                }else{ $image = ''; }
+                $form .= $image;
+            }
+            
+            if( $planet === 'Grand Tour' ){
+                foreach( static::$planets as $planet2 => $attribs ){
+                    if( $planet2 === 'Grand Tour' ){ continue; }
+                    $image = static::$images_root.'/ribbons/shield/'.$planet2.' Visit.png';
+                    if( ! is_readable($image) ){
+                        $image = static::$images_root.'/ribbons/shield/'.$planet2.'Visit.png';
+                    }
+                    if( is_readable($image) && ! is_dir($image) ){
+                        $display = ' display:none;';
+//                        $display = '';
+                        if(
+                            @$selected // Check for default or posted value.
+                        ){ $display = ''; }
+                        $image = '
+            <img alt="'.$image.'" src="'.$image.'" style="position:absolute;top:0;left:0;'.$display.'"/>';
+                    }else{ $image = ''; }
+                    $form .= $image;
+                }
+                
+                $i=1;while($i <= 8){
+                    foreach( array('Orbit','Landing') as $each ){
+                        foreach( array('',' Silver') as $each2 ){
+                            $image = static::$images_root.'/ribbons/shield/'.$each.' '.$i.$each2.'.png';
+                            if( is_readable($image) && ! is_dir($image) ){
+                                $display = ' display:none;';
+//                                $display = '';
+                                if(
+                                    @$selected // Check for default or posted value.
+                                ){ $display = ''; }
+                                $image = '
+            <img alt="'.$image.'" src="'.$image.'" style="position:absolute;top:0;left:0;'.$display.'"/>';
+                            }else{ $image = ''; }
+                            $form .= $image;
+                        }unset($image,$display);
+                    }
+                    $i++;
+                }
+            }
+            
+            // END Ribbon guts.
+            
+            $form .= '
+            <span>'.$planet.'</span>
+        </div>';
+        }
+        $form .= '
+</div>';
+        return $form;
+    }
+    
+}
+?>
